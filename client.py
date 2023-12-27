099e992067749310087f9a33b9f0a47f75ccc3e6
Yousif Ahmed <ya6060123@gmailcom>
2023-12-27 22:32:35 +0300

    Multithreading

--- client.py
+++ client.py
@@ -1,66 +1,69 @@
 import socket
 import json
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as cs:
- cs.connect(("127.0.0.1",49994))
- msg = input(":" )
- cs.send(msg.encode('ascii'))
- while True: 
+try:
+ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as cs:
+  cs.connect(("127.0.0.1",49994))
+  msg = input(":" )
+  cs.send(msg.encode('ascii'))
 
-  print("\nOptions:") 
-  print("a. Arrived flights")
-  print("b. Delayed flights")
-  print("c. Incoming Flights from a city")
-  print("d. Details of a flight")
-  print("e. Quit")
-  
-  option=input(msg+ " Select an option: ")
-  if option =='a':
-   cs.send(option.encode("ascii"))
-   recv=cs.recv(20000)
-   words = json.loads(recv.decode("ascii"))
-   for flight in words["flight"]:
-    #print(flight)
-    print(f"iata:{flight[0]}, Dep_airport:{flight[1]}, Arr_time:{flight[2]}, Terminal:{flight[3]}, Gate:{flight[3]}")
-  
-  
-  if option =='b':
-   cs.send(option.encode("ascii"))
-   recv=cs.recv(20000)
-   words = json.loads(recv.decode("ascii"))
-   for flight in words["flight"]:
-    #print(flight)
-    print(f"iata:{flight[0]}, Dep_airport:{flight[1]}, Dep_time:{flight[2]}")
-    print(f"Est_arr_time:{flight[3]}, Delay:{flight[4]}, Terminal:{flight[5]}, Gate:{flight[6]}")
-    print()
-  
-  if option =='c':
-   try:
-    City_code = input("City code (IATA): ")
+  while True: 
+ 
+   print("\nOptions:") 
+   print("a. Arrived flights")
+   print("b. Delayed flights")
+   print("c. Incoming Flights from a city")
+   print("d. Details of a flight")
+   print("e. Quit")
+   
+   option=input(msg+ " Select an option: ")
+   if option =='a':
     cs.send(option.encode("ascii"))
-    cs.send(City_code.encode("ascii"))
     recv=cs.recv(20000)
     words = json.loads(recv.decode("ascii"))
     for flight in words["flight"]:
-      print(f"iata:{flight[0]}, Dep_airport:{flight[1]}, Dep_time:{flight[2]}")
-      print(f"Est_arr_time:{flight[3]}, Dep Gate:{flight[4]}, Arrival Gate:{flight[5]}, Status:{flight[6]}")
-      print()
-   except(KeyError):print(f"No flights coming from:{City_code}")
-  if option =='d':
-   try:
-    flight_code = input("IATA: ")
+     #print(flight)
+     print(f"iata:{flight[0]}, Dep_airport:{flight[1]}, Arr_time:{flight[2]}, Terminal:{flight[3]}, Gate:{flight[3]}")
+   
+   
+   if option =='b':
     cs.send(option.encode("ascii"))
-    cs.send(flight_code.encode("ascii"))
     recv=cs.recv(20000)
     words = json.loads(recv.decode("ascii"))
     for flight in words["flight"]:
-      print(f"iata:{flight[0]}, Dep_airport:{flight[1]}, Dep Gate:{flight[2]}, Dep Terminal:{flight[3]}")
-      print(f"Arr_airport:{flight[4]}, Arr Gate:{flight[5]}, Arr Terminal:{flight[6]}")
-      print(f"Status:{flight[7]}, schedueld dep time:{flight[8]}, schedueld arr time:{flight[9]}")
-   except(KeyError):print(f"No flights with this iata code:{flight_code}")
-  
-  
-  if option =='e':
-   cs.send(option.encode("ascii"))
-   recv=cs.recv(1024).decode("ascii")
-   print(recv) 
-   break
+     #print(flight)
+     print(f"iata:{flight[0]}, Dep_airport:{flight[1]}, Dep_time:{flight[2]}")
+     print(f"Est_arr_time:{flight[3]}, Delay:{flight[4]}, Terminal:{flight[5]}, Gate:{flight[6]}")
+     print()
+   
+   if option =='c':
+    try:
+     City_code = input("City code (IATA): ")
+     cs.send(option.encode("ascii"))
+     cs.send(City_code.encode("ascii"))
+     recv=cs.recv(20000)
+     words = json.loads(recv.decode("ascii"))
+     for flight in words["flight"]:
+       print(f"iata:{flight[0]}, Dep_airport:{flight[1]}, Dep_time:{flight[2]}")
+       print(f"Est_arr_time:{flight[3]}, Dep Gate:{flight[4]}, Arrival Gate:{flight[5]}, Status:{flight[6]}")
+       print()
+    except(KeyError):print(f"No flights coming from:{City_code}")
+   if option =='d':
+    try:
+     flight_code = input("IATA: ")
+     cs.send(option.encode("ascii"))
+     cs.send(flight_code.encode("ascii"))
+     recv=cs.recv(20000)
+     words = json.loads(recv.decode("ascii"))
+     for flight in words["flight"]:
+       print(f"iata:{flight[0]}, Dep_airport:{flight[1]}, Dep Gate:{flight[2]}, Dep Terminal:{flight[3]}")
+       print(f"Arr_airport:{flight[4]}, Arr Gate:{flight[5]}, Arr Terminal:{flight[6]}")
+       print(f"Status:{flight[7]}, schedueld dep time:{flight[8]}, schedueld arr time:{flight[9]}")
+    except(KeyError):print(f"No flights with this iata code:{flight_code}")
+   
+   
+   if option =='e':
+    cs.send(option.encode("ascii"))
+    recv=cs.recv(1024).decode("ascii")
+    print(recv) 
+    break
+except(ConnectionRefusedError):print("Make sure to start the server first.") 
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import requests
 import json
+import threading
 
 airport_code = input("Choose an airport (icao) code: ")
 
@@ -12,128 +13,141 @@ api_response = requests.get('http://api.aviationstack.com/v1/flights?',params)
 
 with open("group_GC1.json", mode="w") as jsonfile:
   json.dump(api_response.json(),jsonfile, indent=4)
+  data_loaded= True
 
 with open("group_GC1.json", mode="r") as f:
   data = json.load(f)
 print("Server is ready to accept connection from clients")
 ###############################################################################
+
+
+#cs, sockaddress = ss.accept()
+def connection(cs,sockaddress):
+    
+    client_name = {sockaddress:cs.recv(1024).decode('ascii')}
+    print(f"{client_name[sockaddress]} has connected, From {sockaddress[0]} with port number {sockaddress[1]}.")
+
+    while True:
+     try:
+      count=0
+      msg = (cs.recv(1024)).decode('ascii')
+      if msg== 'bye'or msg== 'quit'or msg=="e":
+           cs.send(("disconnecting from the server.").encode('ascii'))
+           print(f'{client_name[sockaddress]} has disconnected.')
+           break
+      if msg =='a'or msg=='arrived':
+         mylist=[]
+         dictionary= {}
+         for flight in data["data"]:
+         
+          if flight["flight_status"]=="landed":
+           response=[]
+           fiata=flight["flight"]["iata"]
+           fdport=flight["departure"]["airport"]
+           farrtime=flight["arrival"]["actual"]
+           farrt=flight["arrival"]["terminal"]
+           farrg=flight["arrival"]["gate"]
+           
+           response.extend([fiata,fdport,farrtime,farrt,farrg])
+           
+           mylist.append(response)
+           dictionary["flight"]=mylist
+           count+=1
+         cs.sendall((json.dumps(dictionary)).encode('ascii'))
+           
+         print((client_name[sockaddress]), "requested all arrived flights.") 
+      if msg=='b':
+         mylist=[]
+         dictionary= {}
+         for flight in data["data"]:
+         
+          if flight["arrival"]["delay"]:
+           response=[]
+           fiata=flight["flight"]["iata"]
+           fdport=flight["departure"]["airport"]
+           fdtime=flight["departure"]["scheduled"]
+           farrtime=flight["arrival"]["estimated"]
+           farrdelay=flight["arrival"]["delay"]
+           farrt=flight["arrival"]["terminal"]
+           farrg=flight["arrival"]["gate"]
+           
+           response.extend([fiata,fdport,fdtime,farrtime,farrdelay,farrt,farrg])
+           
+           mylist.append(response)
+           dictionary["flight"]=mylist
+           count+=1
+         cs.sendall((json.dumps(dictionary)).encode('ascii'))
+           
+         print((client_name[sockaddress]), "requested all delayed flights.") 
+     
+      if msg=='c':
+         mylist=[]
+         dictionary= {}
+         City_code = (cs.recv(1024)).decode('ascii')
+         for flight in data["data"]:
+         
+          if flight["departure"]["iata"]== City_code:
+           response=[]
+           fiata=flight["flight"]["iata"]
+           fdport=flight["departure"]["airport"]
+           fdtime=flight["departure"]["scheduled"]
+           farrtime=flight["arrival"]["estimated"]
+           fdg=flight["departure"]["gate"]
+           farrg=flight["arrival"]["gate"]
+           fstatus= flight["flight_status"]
+           response.extend([fiata,fdport,fdtime,farrtime,fdg,farrg,fstatus])
+           
+           mylist.append(response)
+           dictionary["flight"]=mylist
+           count+=1
+         cs.sendall((json.dumps(dictionary)).encode('ascii'))
+           
+         print((client_name[sockaddress]), "requested flights from:", City_code )
+         
+      if msg=='d':
+         mylist=[]
+         dictionary= {}
+         flight_code = (cs.recv(1024)).decode('ascii') 
+         for flight in data["data"]:
+          if flight["flight"]["iata"]== flight_code:
+          
+           response=[]
+           fiata=flight["flight"]["iata"]
+           fdport=flight["departure"]["airport"]
+           fdg=flight["departure"]["gate"]
+           fdt=flight["departure"]["terminal"]
+           farrport=flight["arrival"]["airport"]
+           farrg=flight["arrival"]["gate"]
+           farrt=flight["arrival"]["terminal"]
+           fstatus= flight["flight_status"]
+           fdtime=flight["departure"]["scheduled"]
+           farrtime=flight["arrival"]["scheduled"]
+           
+           response.extend([fiata,fdport,fdg,fdt,farrport,farrg,farrt,fstatus,fdtime,farrtime])
+           
+           mylist.append(response)
+           dictionary["flight"]=mylist
+           count+=1
+         cs.sendall((json.dumps(dictionary)).encode('ascii'))
+           
+         print((client_name[sockaddress]), "requested details of the flight:", flight_code) 
+    
+    
+     except (socket.error, ConnectionResetError):
+                print(f"{client_name[sockaddress]} has disconnected.")
+                break
+    
+    cs.close()
+
+data_loaded=False
+###############################################################################
 ss = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ss.bind(("127.0.0.1", 49994))
-ss.listen(1)
-cs, sockaddress = ss.accept()
-client = {sockaddress:cs.recv(1024).decode('ascii')}
-print('Accepted request from', sockaddress[0] ,'with port number', sockaddress[1])
-print(f'{client[sockaddress]} has connected.')
+ss.listen(2)
 while True:
- try:
-  count=0
-  msg = (cs.recv(1024)).decode('ascii')
-  if msg== 'bye'or msg== 'quit'or msg=="e":
-       cs.send(("disconnecting from the server.").encode('ascii'))
-       print(f'{client[sockaddress]} has disconnected.')
-       break
-  if msg =='a'or msg=='arrived':
-     mylist=[]
-     dictionary= {}
-     for flight in data["data"]:
-     
-      if flight["flight_status"]=="landed":
-       response=[]
-       fiata=flight["flight"]["iata"]
-       fdport=flight["departure"]["airport"]
-       farrtime=flight["arrival"]["actual"]
-       farrt=flight["arrival"]["terminal"]
-       farrg=flight["arrival"]["gate"]
-       
-       response.extend([fiata,fdport,farrtime,farrt,farrg])
-       
-       mylist.append(response)
-       dictionary["flight"]=mylist
-       count+=1
-     cs.sendall((json.dumps(dictionary)).encode('ascii'))
-       
-     print((count))
-  if msg=='b':
-     mylist=[]
-     dictionary= {}
-     for flight in data["data"]:
-     
-      if flight["arrival"]["delay"]:
-       response=[]
-       fiata=flight["flight"]["iata"]
-       fdport=flight["departure"]["airport"]
-       fdtime=flight["departure"]["scheduled"]
-       farrtime=flight["arrival"]["estimated"]
-       farrdelay=flight["arrival"]["delay"]
-       farrt=flight["arrival"]["terminal"]
-       farrg=flight["arrival"]["gate"]
-       
-       response.extend([fiata,fdport,fdtime,farrtime,farrdelay,farrt,farrg])
-       
-       mylist.append(response)
-       dictionary["flight"]=mylist
-       count+=1
-     cs.sendall((json.dumps(dictionary)).encode('ascii'))
-       
-     print((count))
+ #ss.listen(2)
+ cs, sockaddress = ss.accept()
+ client_Thread = threading.Thread(target=connection, args=(cs,sockaddress))
+ client_Thread.start()
  
-  if msg=='c':
-     mylist=[]
-     dictionary= {}
-     City_code = (cs.recv(1024)).decode('ascii')
-     for flight in data["data"]:
-     
-      if flight["departure"]["iata"]== City_code:
-       response=[]
-       fiata=flight["flight"]["iata"]
-       fdport=flight["departure"]["airport"]
-       fdtime=flight["departure"]["scheduled"]
-       farrtime=flight["arrival"]["estimated"]
-       fdg=flight["departure"]["gate"]
-       farrg=flight["arrival"]["gate"]
-       fstatus= flight["flight_status"]
-       response.extend([fiata,fdport,fdtime,farrtime,fdg,farrg,fstatus])
-       
-       mylist.append(response)
-       dictionary["flight"]=mylist
-       count+=1
-     cs.sendall((json.dumps(dictionary)).encode('ascii'))
-       
-     print((count))
-     
-  if msg=='d':
-     mylist=[]
-     dictionary= {}
-     flight_code = (cs.recv(1024)).decode('ascii') 
-     for flight in data["data"]:
-      if flight["flight"]["iata"]== flight_code:
-      
-       response=[]
-       fiata=flight["flight"]["iata"]
-       fdport=flight["departure"]["airport"]
-       fdg=flight["departure"]["gate"]
-       fdt=flight["departure"]["terminal"]
-       farrport=flight["arrival"]["airport"]
-       farrg=flight["arrival"]["gate"]
-       farrt=flight["arrival"]["terminal"]
-       fstatus= flight["flight_status"]
-       fdtime=flight["departure"]["scheduled"]
-       farrtime=flight["arrival"]["scheduled"]
-       
-       response.extend([fiata,fdport,fdg,fdt,farrport,farrg,farrt,fstatus,fdtime,farrtime])
-       
-       mylist.append(response)
-       dictionary["flight"]=mylist
-       count+=1
-     cs.sendall((json.dumps(dictionary)).encode('ascii'))
-       
-     print((count)) 
-
-
- except (socket.error, ConnectionResetError):
-            print(f"{client[sockaddress]} has disconnected.")
-            break
-
-cs.close()
-ss.close()
-###############################################################################
+#ss.close()
