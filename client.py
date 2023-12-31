ccea41beb52dfa34c178447a68f2dbd7775a6d90
yousif6060 <121193597+yousif6060@users.noreply.github.com>
2024-01-01 02:35:54 +0300

    Add files via upload
    demo ready

---
+++ client.py
@@ -0,0 +1,118 @@
+#Yousif Ahmed Jassim 202010375 sec 1
+#Hasan Ali Khalil Rajab 202100351 sec 1
+import socket
+import json
+from prettytable import PrettyTable
+
+def send_receive_data(client_socket, type, parameter=""):
+#Function to send the request to the server and receive the processed response
+    #if type == "c" or type == "d":
+    type = type + parameter        #ex: cBAH
+    client_socket.send(type.encode("ascii"))
+    server_response = client_socket.recv(32768)
+    processed_response = json.loads(server_response.decode("ascii"))
+    return processed_response
+
+###############################################################################
+def print_flight_details():
+    # Function to print flight details in a table based on the selected option
+    flight_count = len(server_response['flight'])
+    table = PrettyTable()
+    if option == "a":
+        type = "have arrived"
+        table.field_names = ["IATA", "Dep Airport", "Arr Time",
+                              "Terminal", "Gate"]    #table header
+    
+    elif option == "b":
+        type = "have been delayed"
+        table.field_names = ["IATA", "Dep Airport", "Dep Time",
+                              "Est Arr Time", "Delay", "Terminal", "Gate"]   
+    
+    elif option == 'c': 
+        if flight_count == 0:
+            raise IndexError
+        type = f"coming from {city_code}"
+        table.field_names = ["IATA", "Dep Airport", "Departure Time",
+                              "Est Arr Time", "Departure Gate",
+                                "Arrival Gate", "Flight Status"]
+
+    print(f"\n{flight_count} flights {type}.")
+    for flight in server_response["flight"]:
+        table.add_row(flight)
+    print(table)
+
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        # configure client socket
+        server_ip = "127.0.0.1"
+        server_port = 49994
+        client_socket.connect((server_ip, server_port))
+        client_name = input("Name: ")
+        client_socket.send(client_name.encode('ascii'))
+
+        while True:
+            print("\nOptions:")
+            print("a. Arrived flights")
+            print("b. Delayed flights")
+            print("c. Incoming Flights from a city")
+            print("d. Details of a flight")
+            print("e. Quit")
+            try:
+                option = input(f"{client_name} Select an option (a-e): ").lower()
+
+                if option == 'e':
+                    client_socket.send(option.encode("ascii"))
+                    server_response = client_socket.recv(1024).decode("ascii")
+                    print(server_response)
+                    break
+                
+                elif option in ['a','b']:
+                    server_response = send_receive_data(client_socket, option)
+                    print_flight_details()
+                    
+                elif option == 'c':
+                    city_code = input("City code (Airport IATA code): ").upper()
+                    server_response = send_receive_data(client_socket, option, city_code) 
+                    # error message to display it in exceptoin
+                    error_msg = "\nNo flights coming from: " + city_code
+                    print_flight_details()
+                
+                elif option == 'd':
+                    flight_code = input("IATA: ").upper()
+                    server_response = send_receive_data(client_socket, option, flight_code)
+                    flight_count = len(server_response['flight'])
+                    # error message to display it in exceptoin
+                    error_msg = "\nNo flight with this IATA code: " + flight_code
+                    if flight_count == 0:
+                        raise IndexError
+                    
+                    table = PrettyTable()
+                    #server_response = {"flight":[[iata,..,...,.]
+                    #                            ,[iata,...,..,.,],.,.,.]}
+                    flight = server_response["flight"][0] 
+
+                    table.field_names = ["IATA", flight[0]]
+                    table.add_row(["Departure Airport", flight[1]])
+                    table.add_row(["Departure Gate", flight[2]])
+                    table.add_row(["Departure Terminal", flight[3]])
+                    table.add_row(["Arrival Airport", flight[4]])
+                    table.add_row(["Arrival Gate", flight[5]])
+                    table.add_row(["Arrival Terminal", flight[6]])
+                    table.add_row(["Flight Status", flight[7]])
+                    table.add_row(["Scheduled Departure Time", flight[8]])
+                    table.add_row(["Scheduled Arrival Time", flight[9]])
+                    print(table)
+        
+                else:
+                    print("\nInvalid option choose from (a to e).")  
+                   
+                
+            except KeyboardInterrupt:
+                print(f"\n{client_name} forcibly disconnecting.")
+                break
+            
+            except IndexError:
+                        print(error_msg)
+            
+except ConnectionRefusedError:
+    print("Make sure to start the server first.")
---
+++ server.py
@@ -0,0 +1,156 @@
+#Yousif Ahmed Jassim 202010375 sec 1
+#Hasan Ali Khalil Rajab 202100351 sec 1
+import socket
+import requests
+import json
+import threading
+
+def fix_time(time): 
+     # Function to fix the time format: ("2023-12-31T05:40:00+00:00" --> "5:40")
+    if not time: return "None"   #sometimes [arrival][actual] is null from API
+    time_string = time.split(":")
+    hour = (time_string[0])[-2:]
+    minutes = time_string[1]
+    if hour[0] == "0": 
+        hour = hour[1]
+    return hour + ":" + minutes
+
+def handle_client_request(client_socket, sock_address):
+    # Function to handle client requests
+    client_name = {sock_address: client_socket.recv(1024).decode('ascii')}
+    print(f"{client_name[sock_address]} has connected, "
+          f"From (ip, port):({sock_address[0]}, {sock_address[1]}).")
+
+    while True:
+        try: 
+            # Receive client request
+            request = client_socket.recv(1024).decode('ascii')
+            # If request is 'e', send disconnection message and break the loop
+            if request == "e":
+                client_socket.send(("disconnecting from the server.").encode('ascii'))
+                print(f'{client_name[sock_address]} has disconnected.')
+                break
+            
+            response_json = {}
+            response_list = []
+
+            if request == 'a': # Request for all arrived flights
+                for flight in data["data"]:
+                    if flight["flight_status"] == "landed":
+                        response = [
+                            flight["flight"]["iata"],
+                            flight["departure"]["airport"],
+                            fix_time(flight["arrival"]["actual"]),
+                            flight["arrival"]["terminal"],
+                            flight["arrival"]["gate"]
+                        ]
+                        response_list.append(response)
+                print((client_name[sock_address]), "requested all arrived flights.")
+            
+            
+            elif request == 'b': # Request for all delayed flights
+                for flight in data["data"]:
+                    if flight["arrival"]["delay"]:
+                        response = [
+                            flight["flight"]["iata"],
+                            flight["departure"]["airport"],
+                            fix_time(flight["departure"]["scheduled"]),
+                            fix_time(flight["arrival"]["estimated"]),
+                            flight["arrival"]["delay"],
+                            flight["arrival"]["terminal"],
+                            flight["arrival"]["gate"]
+                        ]
+                        response_list.append(response)
+                print((client_name[sock_address]), "requested all delayed flights.")
+            
+            elif request[0] == 'c': # Request for flights from a specific city code
+                city_code = request[1:]
+                for flight in data["data"]:
+                    if flight["departure"]["iata"] == city_code:
+                        response = [
+                            flight["flight"]["iata"],
+                            flight["departure"]["airport"],
+                            fix_time(flight["departure"]["scheduled"]),
+                            fix_time(flight["arrival"]["estimated"]),
+                            flight["departure"]["gate"],
+                            flight["arrival"]["gate"],
+                            flight["flight_status"]
+                        ]
+                        response_list.append(response)
+
+                print((client_name[sock_address]), "requested flights from:", city_code)
+            
+            elif request[0] == 'd': # Request for details of a specific flight
+                flight_code = request[1:]
+                for flight in data["data"]:
+                    if flight["flight"]["iata"] == flight_code:
+                        response = [
+                            flight["flight"]["iata"],
+                            flight["departure"]["airport"],
+                            flight["departure"]["gate"],
+                            flight["departure"]["terminal"],
+                            flight["arrival"]["airport"],
+                            flight["arrival"]["gate"],
+                            flight["arrival"]["terminal"],
+                            flight["flight_status"],
+                            fix_time(flight["departure"]["scheduled"]),
+                            fix_time(flight["arrival"]["scheduled"])
+                        ]
+                        response_list.append(response)
+                print((client_name[sock_address]),
+                       "requested details of the flight:", flight_code)
+            
+            response_json["flight"] = response_list
+            # sending the response in json format
+            client_socket.sendall((json.dumps(response_json)).encode('ascii'))
+            
+        except (socket.error, ConnectionResetError,IndexError): 
+            # Catch socket errors and handle disconnection
+            print(f"{client_name[sock_address]} has disconnected.")
+            break
+    
+    client_socket.close()
+
+###############################################################################
+# Get airport icao code from user
+airport_code = input("Choose an airport (ICAO) code: ").upper()
+params = {
+    'access_key': 'dcde146b78bbf011a305dcb511911ddf',
+    'limit': 100,
+    'arr_icao': airport_code
+}
+# Make API request to retrieve flight data
+api_response = requests.get('http://api.aviationstack.com/v1/flights?', params)
+
+
+# Save API response to a JSON file
+with open("group_GC1.json", "w") as json_file:
+    json.dump(api_response.json(), json_file, indent=4)
+
+# Load flight data from JSON file
+with open("group_GC1.json", "r") as f:
+    data = json.load(f)
+
+if api_response.status_code != 200:
+    #display an error massage in case the api fail
+    print("Api error, run the server and try again.")
+    print(data["error"]["message"])
+    quit()
+else:
+    print("Server is ready to accept connections from clients")
+
+###############################################################################
+# configure server socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_ip = "127.0.0.1"
+server_port = 49994
+server_socket.bind((server_ip, server_port))
+server_socket.listen(3)
+
+while True:
+    # Accept client connections
+    client_socket, sock_address = server_socket.accept()
+    # Create a new thread for each client
+    client_Thread = threading.Thread(target=handle_client_request,
+                                     args=(client_socket, sock_address))
+    client_Thread.start()
